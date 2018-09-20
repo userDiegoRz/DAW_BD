f6b0635ab204bd01615a2cc4161c07b5fbf93c6c
DiegoRz <diego_rz2@hotmail.com>
2018-09-20 15:28:05 -0500

    Terminar funcionalidad de lab 11

--- Lab11/index.php
+++ Lab11/index.php
@@ -2,7 +2,6 @@
     
     include("_header.html");
     include("_login_form.html");
-    include("_body.html");
     include("_footer.html");
 
 ?>
---
+++ Lab11/validar.php
@@ -0,0 +1,37 @@
+<?php
+    
+    if (isset($_POST["usuario"])) {
+        $_POST["usuario"] = htmlspecialchars($_POST["usuario"]);
+    }
+
+    if ($_POST["usuario"]=="Profesor" && $_POST["clave1"]=="Pass01"
+        && $_POST["clave2"] == "Pass01") {
+        include("_header.html");
+        echo "<h2> Bienvenido ".$_POST["usuario"]."</h2>";
+        include("_body.html");
+        include("_footer.html");
+    } else if ($_POST["usuario"]=="Becario" && $_POST["clave1"]=="_password"
+        && $_POST["clave2"] == "_password") {
+        include("_header.html");
+        echo "<h2> Bienvenido ".$_POST["usuario"]."</h2>";
+        include("_body.html");
+        include("_footer.html");
+    } else if ($_POST["usuario"]=="" || $_POST["clave1"]==""
+        || $_POST["clave2"] == "") {
+        include("_header.html");
+        $error = "<p>Datos <strong>INVALIDOS</strong>.</p>";
+        include("_login_form.html");
+        include("_footer.html");
+        
+    }else {
+       include("_header.html");
+        $error = "<p>Usuario <strong>NO REGISTRADO</strong>.</p>";
+        include("_login_form.html");
+        include("_footer.html");
+    }
+        
+    $usuario = $clave1 = $clave2 = "";
+        
+        
+
+?>
